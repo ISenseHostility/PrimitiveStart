182333eed1706aab8bf6ab48b9b0df719ee3ed53
ISenseHostility <yaboijarno@gmail.com>
2023-02-13 23:56:07 +0100

    Integrated forge tags, copper tools can now only mine up to iron ore

--- src/main/java/isensehostility/primitivestart/init/ItemInit.java
+++ src/main/java/isensehostility/primitivestart/init/ItemInit.java
@@ -25,11 +25,11 @@ public class ItemInit {
     public static final RegistryObject<Item> BONE_SHEARS = ITEMS.register("bone_shears", () -> new ShearsItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(95)));
     public static final RegistryObject<Item> BONE_ARROW = ITEMS.register("bone_arrow", () -> new BoneArrowItem(1.0F));
 
-    public static final RegistryObject<Item> REINFORCED_BONE_SWORD = ITEMS.register("reinforced_bone_sword", () -> new SwordItem(Tiers.IRON, 3, -2.4F, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(190)));
-    public static final RegistryObject<Item> REINFORCED_BONE_PICKAXE = ITEMS.register("reinforced_bone_pickaxe", () -> new PickaxeItem(Tiers.IRON, 1, -2.8F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
-    public static final RegistryObject<Item> REINFORCED_BONE_AXE = ITEMS.register("reinforced_bone_axe", () -> new AxeItem(Tiers.IRON, 6, -3.1F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
-    public static final RegistryObject<Item> REINFORCED_BONE_SHOVEL = ITEMS.register("reinforced_bone_shovel", () -> new ShovelItem(Tiers.IRON, 1.5F, -3.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
-    public static final RegistryObject<Item> REINFORCED_BONE_HOE = ITEMS.register("reinforced_bone_hoe", () -> new HoeItem(Tiers.IRON, -2, -1.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
+    public static final RegistryObject<Item> REINFORCED_BONE_SWORD = ITEMS.register("reinforced_bone_sword", () -> new SwordItem(PrimitiveStart.COPPER, 3, -2.4F, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(190)));
+    public static final RegistryObject<Item> REINFORCED_BONE_PICKAXE = ITEMS.register("reinforced_bone_pickaxe", () -> new PickaxeItem(PrimitiveStart.COPPER, 1, -2.8F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
+    public static final RegistryObject<Item> REINFORCED_BONE_AXE = ITEMS.register("reinforced_bone_axe", () -> new AxeItem(PrimitiveStart.COPPER, 6, -3.1F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
+    public static final RegistryObject<Item> REINFORCED_BONE_SHOVEL = ITEMS.register("reinforced_bone_shovel", () -> new ShovelItem(PrimitiveStart.COPPER, 1.5F, -3.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
+    public static final RegistryObject<Item> REINFORCED_BONE_HOE = ITEMS.register("reinforced_bone_hoe", () -> new HoeItem(PrimitiveStart.COPPER, -2, -1.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
 
     public static final RegistryObject<Item> REINFORCED_BONE_SHEARS = ITEMS.register("reinforced_bone_shears", () -> new ShearsItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
 
