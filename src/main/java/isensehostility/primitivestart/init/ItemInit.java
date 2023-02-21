6569e121d7c52e5c8d90d3e82c687f86dca83c42
ISenseHostility <yaboijarno@gmail.com>
2023-02-21 01:09:04 +0100

    Fixed plated tools tier

--- src/main/java/isensehostility/primitivestart/init/ItemInit.java
+++ src/main/java/isensehostility/primitivestart/init/ItemInit.java
@@ -36,11 +36,11 @@ public class ItemInit {
 
     public static final RegistryObject<Item> REINFORCED_BONE_ARROW = ITEMS.register("reinforced_bone_arrow", () -> new ReinforcedBoneArrowItem(1.5F));
 
-    public static final RegistryObject<Item> PLATED_BONE_SWORD = ITEMS.register("plated_bone_sword", () -> new SwordItem(PrimitiveStart.COPPER, 3, -2.4F, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(200)));
-    public static final RegistryObject<Item> PLATED_BONE_PICKAXE = ITEMS.register("plated_bone_pickaxe", () -> new PickaxeItem(PrimitiveStart.COPPER, 1, -2.8F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
-    public static final RegistryObject<Item> PLATED_BONE_AXE = ITEMS.register("plated_bone_axe", () -> new AxeItem(PrimitiveStart.COPPER, 6, -3.1F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
-    public static final RegistryObject<Item> PLATED_BONE_SHOVEL = ITEMS.register("plated_bone_shovel", () -> new ShovelItem(PrimitiveStart.COPPER, 1.5F, -3.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
-    public static final RegistryObject<Item> PLATED_BONE_HOE = ITEMS.register("plated_bone_hoe", () -> new HoeItem(PrimitiveStart.COPPER, -2, -1.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
+    public static final RegistryObject<Item> PLATED_BONE_SWORD = ITEMS.register("plated_bone_sword", () -> new SwordItem(PrimitiveStart.PLATED, 3, -2.4F, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).stacksTo(1).durability(200)));
+    public static final RegistryObject<Item> PLATED_BONE_PICKAXE = ITEMS.register("plated_bone_pickaxe", () -> new PickaxeItem(PrimitiveStart.PLATED, 1, -2.8F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
+    public static final RegistryObject<Item> PLATED_BONE_AXE = ITEMS.register("plated_bone_axe", () -> new AxeItem(PrimitiveStart.PLATED, 6, -3.1F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
+    public static final RegistryObject<Item> PLATED_BONE_SHOVEL = ITEMS.register("plated_bone_shovel", () -> new ShovelItem(PrimitiveStart.PLATED, 1.5F, -3.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
+    public static final RegistryObject<Item> PLATED_BONE_HOE = ITEMS.register("plated_bone_hoe", () -> new HoeItem(PrimitiveStart.PLATED, -2, -1.0F, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(200)));
 
     public static final RegistryObject<Item> PLATED_BONE_SHEARS = ITEMS.register("plated_bone_shears", () -> new ShearsItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(1).durability(190)));
 
